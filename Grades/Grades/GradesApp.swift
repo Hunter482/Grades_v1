83ba90e0190ff232aa0fe17d668498e6457330ea
Hunter482 <161045593+Hunter482@users.noreply.github.com>
2024-02-26 17:58:46 +0100

    Add files via upload

---
+++ Grades/Grades/GradesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradesApp.swift
+//  Grades
+//
+//  Created by Jakob Erlebach on 26.02.24.
+//
+
+import SwiftUI
+
+@main
+struct GradesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
